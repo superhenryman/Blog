745583329d659a266f9fb544b05f423fbf9cd3cb
superhenryman <kareem.a.alhaj@gmail.com>
2026-01-31 21:23:53 +0300

    yay

--- static/admin_login.js
+++ static/admin_login.js
@@ -1,6 +1,7 @@
 const form = document.getElementById("login_form");
 const error = document.getElementById("error");
 form.addEventListener("submit", async (event)=> {
+    error.style.display = "none";
     event.preventDefault();
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
@@ -16,8 +17,10 @@ form.addEventListener("submit", async (event)=> {
     if (response.ok) {
         window.location.href = "/admin_panel";
     } else if (response.status == 401) {
+        error.style.display = "block";
         error.innerText = "Incorrect username or password.";
     } else {
+        error.style.display = "block";
         error.innerText = "Internal server error.";
     }
 });
