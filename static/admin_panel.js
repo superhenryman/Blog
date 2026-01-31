9c3d06ae72333faff48058d662b800bc63ca82dd
superhenryman <kareem.a.alhaj@gmail.com>
2026-01-31 21:49:00 +0300

    adding rate limited to exceptions

--- static/admin_panel.js
+++ static/admin_panel.js
@@ -16,11 +16,14 @@ form.addEventListener("submit", async (e) => {
                 "post": post.value
         })
         });
-        if (!response.ok) {
-            alert("An error has occured creating a post!");
+        if (response.status = 429) {
+            alert("You've been rate limited. Try uploading in the next 1 minute.");
+            submitted = false;
+        } else if (response.ok) {
+            document.getElementById("success").innerText = "Post uploaded!";
             submitted = true;
         } else {
-            document.getElementById("success").innerText = "Post uploaded!";
+            alert("An error has occured in the server.");
             submitted = true;
         }
     } else {
